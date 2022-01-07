9851d57f970fb08ed7a6bd846d643496a87089d5
Ayush Sharma <ayush20892@gmail.com>
2022-01-07 23:16:47 +0530

    Cookie Same Site Update.

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -4,6 +4,7 @@ module.exports = async (user, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRY * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: "none",
   };
 
   user.password = undefined;
