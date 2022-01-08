ba92e7616f3c3f9a133f7161af059d7637891c4b
Ayush Sharma <ayush20892@gmail.com>
2022-01-09 00:12:23 +0530

    Demo Site Cors Update

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const cookieParser = require("cookie-parser");
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://unusual-ecom.netlify.app/"],
+    origin: [
+      "http://localhost:3000",
+      "https://unusual-ecom.netlify.app/",
+      "https://deploy-preview-2--unusual-ecom.netlify.app/",
+    ],
     credentials: true,
   })
 );
